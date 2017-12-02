79ec12c1fcaec50fe46020265e26193bb4e43b31
nishchalb <nishchal2050@gmail.com>
2017-12-02 14:16:15 -0500

    added image file stuff

--- mask_classify.py
+++ mask_classify.py
@@ -1,6 +1,14 @@
 import numpy as np
 import skimage
 from skimage.filters import gaussian
+import os
+import glob
+
+IMAGE_DIR = '/home/nishchal/workspace/MIT/6861/ILSVRC2013_DET_val/done'
+MASK_DIR = '/home/nishchal/workspace/MIT/6861/saliency-salgan-2017/imagenet_saliency'
+IMAGE_NAMES = []
+for img_name in glob.glob(os.path.join(MASK_DIR, '*')):
+    IMAGE_NAMES.append(os.path.split(img_name)[-1])
 
 def make_masked_image(image, mask_img, threshold, blur=False, blur_amount=10):
     mask = mask_img <= threshold
@@ -13,5 +21,9 @@ def make_masked_image(image, mask_img, threshold, blur=False, blur_amount=10):
     result[mask] = back_image[mask]
     return result
 
-
-
+def test_image_at_levels(image_name, level_list, blur=False, blur_amount=10):
+    img = skimage.io.imread(os.path.join(IMAGE_DIR, image_name))
+    mask_img = skimage.io.imread(os.path.join(MASK_DIR, image_name))
+    for level in level_list:
+        masked_image = make_masked_image(img, mask_img, level, blur, blur_amount)
+        # TODO: VGG classify stuff, store results
