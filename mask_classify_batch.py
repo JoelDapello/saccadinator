5784646916958bfcd23bed4d8aba39c770cec662
Nishchal Bhandari <nishchal2050@gmail.com>
2017-12-05 23:14:51 -0500

    added transpose failure case, and intermediate saving

--- mask_classify_batch.py
+++ mask_classify_batch.py
@@ -24,6 +24,8 @@ import json
 # In[33]:
 
 def make_masked_image(image, mask_img, threshold, blur=False, blur_amount=10):
+    if (image.shape[:2] != mask_img.shape):
+        mask_img = mask_img.transpose()
     mask = mask_img < threshold
     result = image.copy()
     if blur:
@@ -71,7 +73,7 @@ def test_image_at_levels(image_name, percentages, model, blur=False, blur_amount
     img = cv2.imread(os.path.join(IMAGE_DIR, image_name[:-3]+'JPEG'))
 #     plt.imshow(img)
     # mask_img = skimage.io.imread(os.path.join(MASK_DIR, image_name))
-    mask_img = cv2.imread(os.path.join(MASK_DIR, image_name))
+    mask_img = cv2.imread(os.path.join(MASK_DIR, image_name), 0)
     level_list = get_ntiles_for_img(mask_img, percentages)
 #     print(level_list)
     for level in level_list:
@@ -122,7 +124,7 @@ def get_VGG_map(WNID):
 
 # Make VGG model
 # VGG_WEIGHTS = '/n/regal/cox_lab/dapello/VGG16_Keras_TensorFlow/data/model/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
-VGG_WEIGHTS = '/home/nishchal/workspace/MIT/6861/saccadinator/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
+VGG_WEIGHTS = '/home/nishchal/workspace/saccadinator/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
 model = VGG_16(VGG_WEIGHTS)
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer=sgd, loss='categorical_crossentropy')
@@ -131,9 +133,9 @@ model.compile(optimizer=sgd, loss='categorical_crossentropy')
 # In[4]:
 
 # IMAGE_DIR = '/n/regal/cox_lab/dapello/saliency-salgan-2017/images/ILSVRC2012_val/'
-IMAGE_DIR = '/home/nishchal/workspace/MIT/6861/ILSVRC2012_val/'
+IMAGE_DIR = '/home/nishchal/workspace/imagenet/'
 # MASK_DIR = '/n/regal/cox_lab/dapello/saliency-salgan-2017/saliency/ILSVRC2012_val/'
-MASK_DIR = '/home/nishchal/workspace/MIT/6861/saliency-salgan-2017/imagenet_saliency/'
+MASK_DIR = '/home/nishchal/workspace/saliency'
 IMAGE_NAMES = []
 # VAL_LABELS = [line.replace('\n','') for line in open('ground_truth_sane.txt')]
 with open('ground_truth.json') as fp:
@@ -141,15 +143,20 @@ with open('ground_truth.json') as fp:
 
 for img_name in glob.glob(os.path.join(MASK_DIR, '*')):
     IMAGE_NAMES.append(os.path.split(img_name)[-1])
+IMAGE_NAMES = sorted(IMAGE_NAMES)
 
 
 # In[34]:
 
 results_array = []
 ntiles = np.arange(0,101,10)
-for image_name in IMAGE_NAMES:
+# IMAGE_NAMES = ['ILSVRC2012_val_00040854.jpg']
+for i, image_name in enumerate(IMAGE_NAMES):
     results_dict = test_image_at_levels(image_name, ntiles, model, blur=True)
     results_array.append(results_dict)
+    if i%500==0:
+        results_array_np = np.array(results_array)
+        np.savez_compressed('results_array'+str(i),results_array_np)
 
 results_array_np = np.array(results_array)
 np.savez('results_array',results_array_np)
